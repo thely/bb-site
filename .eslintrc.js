2041e8e576e52a9178a45be5eb035bb32fb5f356
thely <bardbarienne@gmail.com>
2020-09-02 15:37:01 -0400

    fixing eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,17 +8,23 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
-    '@nuxtjs',
-    'prettier',
+    'plugin:vue/recommended',
+    // 'prettier',
+    'eslint:recommended',
     'prettier/vue',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    // 'plugin:nuxt/recommended'
   ],
   plugins: [
-    'prettier'
+    'prettier', 'vue'
   ],
+  globals: {
+    $nuxt: true,
+  },
   // add your custom rules here
   rules: {
     "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   }
 }
